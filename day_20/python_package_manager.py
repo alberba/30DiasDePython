407c891c2bdce322936d53555ba826dcbddd4e86
alberba <83814585+alberba@users.noreply.github.com>
2023-07-04 11:05:47 +0200

    Day 20

--- day_20/python_package_manager.py
+++ day_20/python_package_manager.py
@@ -41,4 +41,41 @@ print(mean_weight)
 print(median_weight)
 print(standard_deviation)
 # b) the min, max, mean, median, standard deviation of cats' lifespan in years.
+arr_life_span = []
+for cat  in cats_json:
+    range_life_span = cat["life_span"].split(" - ")
+    range_life_span = [int(i) for i in range_life_span]
+    arr_life_span += range_life_span
+arr_life_span.sort()
+min_life_span = min(arr_life_span)
+max_life_span = max(arr_life_span)
+mean_life_span = sum(arr_life_span) / len(arr_life_span)
+if len(arr_life_span) % 2 == 0:
+    median_life_span = (arr_life_span[int(len(arr_life_span) / 2) - 1] + arr_life_span[int(len(arr_life_span) / 2)]) / 2
+else:
+    median_life_span = arr_life_span[int(len(arr_life_span) / 2)]
+deviation = [(x - mean_life_span) ** 2 for x in arr_life_span]
+standard_deviation = (sum(deviation) / len(arr_life_span)) ** 0.5
+
+print(min_life_span)
+print(max_life_span)
+print(mean_life_span)
+print(median_life_span)
+print(standard_deviation)
 # c) Create a frequency table of country and breed of cats
+frequency_country = {}
+for cat  in cats_json:
+    frequency_country[cat["origin"]] = frequency_country.get(cat["origin"], 0) + 1
+
+sorted_frequency = sorted(frequency_country.items(), key = lambda x: x[1], reverse = True)
+
+print("País\t\t\tRazas")
+for country in sorted_frequency:
+    print("%s\t\t%d" %(country[0], country[1]))
+
+# Read the countries API and find
+countries_api = 'https://restcountries.eu/rest/v2/all'
+countries_json = requests.get(countries_api).json()
+# a) the 10 largest countries
+for country in countries_json:
+    print(country)
